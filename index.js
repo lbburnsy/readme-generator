3059780fe26047652ce8d4ec914b2127140c1b63
Luc Burns <burnsluc2@gmail.com>
2021-03-25 21:34:28 -0400

    Consolidated functions

--- index.js
+++ index.js
@@ -19,39 +19,38 @@ inquirer
       message: "List any necessary installation commands:",
     },
     {
-        type: "input",
-        name: "usage",
-        message: "Provide a relative path to a video walk through of your app in use:"
+      type: "input",
+      name: "usage",
+      message:
+        "Provide a relative path to a video walk through of your app in use:",
     },
     {
-        type: "input",
-        name: "contribution",
-        message: "Where can people go to contribute?"
+      type: "input",
+      name: "contribution",
+      message: "Where can people go to contribute?",
     },
     {
-        type: "input",
-        name: "functionality",
-        message: "Enter instructions to run your app:"
-    }
+      type: "input",
+      name: "functionality",
+      message: "Enter instructions to run your app:",
+    },
   ])
   .then(function (data) {
     console.log(data);
-    writeFile(data.title);
-    appendDescription(data.description);
-    appendInstallation(data.installation);
-    appendUsage(data.usage);
-    appendContribution(data.contribution);
-    appendFunctionality(data.functionality);
+    writeFile(data);
   });
 
-function appendFile(output) {
-    fs.appendFile("README.md", output + "\n", function (err) {
-        if (err) throw err;
-      });
-}
+function writeFile(data) {
+  let output =
+  `# ${data.title}\n` +
+  `## Output \n ${data.description}\n` +
+  "## Installation \n This app relies on certain dependencies that will need to be downloaded to successfully run it. To do this, enter the following command from your terminal:\n```\n" +
+  data.installation +
+  "\n```\n" +
+  `## Usage\n[Video Walk-through]("${data.usage}")\n` +
+  `## Contribute\n${data.contribution}\n` +
+  `## Use this App\n ${data.functionality}\n`;
 
-function writeFile(title) {
-  let output = `# ${title}`;
   fs.writeFile(`README.md`, output + "\n", function (err) {
     if (err) {
       return console.log(err);
@@ -61,30 +60,3 @@ function writeFile(title) {
   });
 }
 
-function appendDescription(description) {
-  let output = `## Output \n ${description}`;
-  fs.appendFile(`README.md`, output + "\n", function (err) {
-    if (err) throw err;
-  });
-}
-
-function appendInstallation(installation) {
-  let output =
-    "## Installation \n This app relies on certain dependencies that will need to be downloaded to successfully run it. To do this, enter the following command from your terminal:\n```\n"+installation+"\n```";
-  appendFile(output);
-}
-
-function appendUsage(usage) {
-    let output = `## Usage\n[Video Walk-through]("${usage}")`
-    appendFile(output);
-}
-
-function appendContribution(contribution) {
-    let output = `## Contribute\n${contribution}`;
-    appendFile(output);
-}
-
-function appendFunctionality(functionality) {
-    let output = `## Use this App\n ${functionality}`;
-    appendFile(output);
-}
